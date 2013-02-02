2eefb144e1901ebab0a584540dd78c82db126d73
Jose Carlos Temprado Morales <thempra@thempra.net>
2013-02-02 13:21:04 +0100

    Added function to download cards file from a XML

--- gen/net/thempra/yocaina/R.java
+++ gen/net/thempra/yocaina/R.java
@@ -23,17 +23,19 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayoutButtons=0x7f070002;
-        public static final int btnDownload=0x7f07000c;
+        public static final int btnDownload=0x7f07000d;
         public static final int btn_clear=0x7f070003;
         public static final int btn_decode=0x7f070004;
         public static final int btn_dump=0x7f070005;
         public static final int btn_other=0x7f070006;
         public static final int cmbCardsDialog=0x7f070000;
+        public static final int linearLayout1=0x7f07000b;
         public static final int status_data=0x7f070009;
         public static final int status_label=0x7f070008;
         public static final int tableStatus=0x7f070007;
         public static final int tblGeneral=0x7f070001;
-        public static final int txtURL=0x7f07000b;
+        public static final int txtDebug=0x7f07000e;
+        public static final int txtURL=0x7f07000c;
         public static final int viewScan=0x7f07000a;
     }
     public static final class layout {
--- src/net/thempra/yocaina/RepositoryActivity.java
+++ src/net/thempra/yocaina/RepositoryActivity.java
@@ -1,58 +1,254 @@
 package net.thempra.yocaina;
 
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.http.util.ByteArrayBuffer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class RepositoryActivity extends Activity
 
 {
-	
+	private static final String ACTION_GET_CARDS = "Get_cards";
+
 	private static EditText txtURL;
 	private static Button btnDownload;
-	
+	private static TextView txtDebug;
+
+
+	ArrayList<String> card;
+
+	ArrayList<HashMap<String, Object>> mylist = new ArrayList<HashMap<String, Object>>();
+	HashMap<String, Object> map;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		//setContentView(R.layout.repository);
+		setContentView(R.layout.repositoryactivity);
 		
 		txtURL= (EditText) findViewById(R.id.txtURL);
 		btnDownload= (Button) findViewById(R.id.btnDownload);
 		
-		
+	
+			
+		card = new ArrayList<String>();
+	    
+	    
+	//	btnDownload.setOnClickListener(new OnClickListener() {
+	//		public void onClick(View view) {
 				
-		try {
-			DownloadFile(  new URL(txtURL.getText().toString()+"/albacete") );
-		} catch (MalformedURLException e) {
+				new AsyncTask<Activity, Void, String>() {
+			        @Override
+			        public String doInBackground(Activity... activities) {
+			        	card= getItemsFromURL("http://"+txtURL.getText().toString()+"/cards.xml","card");
+			     
+			        	//Send to Activity
+			    		Intent conectedIntent = new Intent();
+			    		conectedIntent.setAction(ACTION_GET_CARDS);
+			            sendBroadcast(conectedIntent);
+			            return ""; //return Utilities.DBGetOnlineVersionNumber(activities[0]);
+			        }
+			        
+			    }.execute(this);
+	    
+	    
+		//	 }
+
+	    //});
+
+			    
+	    
+	    if (activityReceiver != null) {
+        	//Create an intent filter to listen to the broadcast sent with the action "ACTION_STRING_ACTIVITY"
+        	            IntentFilter intentFilter = new IntentFilter(ACTION_GET_CARDS);
+        	//Map the intent filter to the receiver
+        	            registerReceiver(activityReceiver, intentFilter);
+        	        }
+	}
+	
+private ArrayList<String> getItemsFromURL(String urlXml, String mainNode){
+		
+		ArrayList<String> data = new ArrayList<String>();
+		
+		try{
+			
+		URL url = new URL(urlXml);
+			
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+       
+        
+        Document doc = db.parse(new InputSource(url.openStream()));
+        doc.getDocumentElement().normalize();
+		
+        NodeList nodeList = doc.getElementsByTagName(mainNode);
+
+        /** Assign textview array lenght by arraylist size */
+        
+        for (int i = 0; i < nodeList.getLength(); i++) {
+
+            
+            Element fstElmnt = (Element) nodeList.item(i);
+            data.add (getNodeValueByTagName(fstElmnt, "card"));
+            
+            map = new HashMap<String, Object>();
+            map.put("name", getNodeValueByTagName(fstElmnt, "name"));
+            map.put("url", getNodeValueByTagName(fstElmnt, "url"));
+            
+     
+            //getResources().getResourceName(resid)
+			mylist.add(map);
+
+        }
+		
+        
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	   
+	
 		
+		
+		return data;
+	    }
+	
+	
+	private String getNodeValueByTagName(Node parentNode, String tagNameOfNode)
+	{
+	    String nodeValue = "";
+	    if (((Element) parentNode).getElementsByTagName(tagNameOfNode).getLength() != 0)
+	        if (((Element) ((Element) parentNode).getElementsByTagName(tagNameOfNode).item(0)).hasChildNodes())
+	        {
+	            nodeValue = ((Node) ((Element) ((Element) parentNode).getElementsByTagName(tagNameOfNode).item(0)).getChildNodes().item(0)).getNodeValue();
+	        }
+	    return nodeValue;
+	}
+
+	private String getNodeAttributeByTagName(Node parentNode, String tagNameOfAttr)
+	{
+	    String nodeValue = "";
+
+	    NamedNodeMap questNodeAttr = parentNode.getAttributes();
+
+	    if (questNodeAttr.getLength() != 0)
+	        nodeValue = questNodeAttr.getNamedItem(tagNameOfAttr).getTextContent();
+
+	    return nodeValue;
 	}
 	
 	
+	private void manageNewCards() {
+		
+		txtDebug = (TextView) findViewById(R.id.txtDebug);
+	    
+	    
+		for (HashMap<String, Object> item : mylist)
+		{
+			txtDebug.setText(txtDebug.getText()+"\n\t- "+item.get("name").toString());
+			try {
+				DownloadFile(item.get("name").toString(),new URL(item.get("url").toString()));
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+			
+	}
+
+	private BroadcastReceiver activityReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+        	manageNewCards();
+        }
+    };
 
 
-	void DownloadFile (URL url) {
+	void DownloadFile (String fileName, URL url) {
+		
 		try {
+           
+            File file = new File(fileName);
+
+            long startTime = System.currentTimeMillis();
+            Log.d("Manage", "download begining");
+            Log.d("Manage", "download url:" + url);
+            Log.d("Manage", "downloaded file name:" + fileName);
+            /* Open a connection to that URL. */
+            URLConnection ucon = url.openConnection();
+
+            
+            ucon.setReadTimeout(5000);
+            ucon.setConnectTimeout(30000);
+            
+            /*
+             * Define InputStreams to read from the URLConnection.
+             */
+            
+            
+            /*************************************************************************/
+            //FIXME: Error al hacer el getInputStream 
+            /*************************************************************************/
+            
+            InputStream is = ucon.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+            /*
+             * Read bytes to the Buffer until there is nothing more to read(-1).
+             */
+            ByteArrayBuffer baf = new ByteArrayBuffer(50);
+            int current = 0;
+            while ((current = bis.read()) != -1) {
+                    baf.append((byte) current);
+            }
+
+            /* Convert the Bytes read to a String. */
+            FileOutputStream fos = new FileOutputStream(Environment.getExternalStorageDirectory().toString()+"/yocaina/"+file);
+            fos.write(baf.toByteArray());
+            fos.close();
+            Log.d("Manage", "download ready in"
+                            + ((System.currentTimeMillis() - startTime) / 1000)
+                            + " sec");
+
+    } catch (IOException e) {
+            Log.d("Manage", "Error: " + e);
+    }
+		
+	/*	try {
 	        //set the download URL, a url that points to a file on the internet
 	        //this is the file to be downloaded
 	       
@@ -73,7 +269,7 @@ public class RepositoryActivity extends Activity
 	        File SDCardRoot = Environment.getExternalStorageDirectory();
 	        //create a new file, specifying the path, and the filename
 	        //which we want to save the file as.
-	        File file = new File(SDCardRoot,"/yocaina/prueba");
+	        File file = new File(SDCardRoot,"/yocaina/"+name);
 	
 	        //this will be used to write the downloaded data into the file we created
 	        FileOutputStream fileOutput = new FileOutputStream(file);
@@ -109,5 +305,7 @@ public class RepositoryActivity extends Activity
 	} catch (IOException e) {
 	        e.printStackTrace();
 	}
+	*/
 	}
+	
 }
